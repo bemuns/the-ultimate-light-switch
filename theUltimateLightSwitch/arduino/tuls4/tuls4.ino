7ba4d4cb6a3147692adae9be92db7fdebd7e90fc
robcb85 <robcb85@gmail.com>
2012-04-17 07:02:07 -0600

    Revert "test"
    This reverts commit f7be0505f54909817247e521be65c3ef13245bfb.

--- theUltimateLightSwitch/arduino/tuls4/tuls4.ino
+++ theUltimateLightSwitch/arduino/tuls4/tuls4.ino
@@ -8,7 +8,6 @@
  * Contributors:
  *     Robert Barnes - initial API and implementation
  ******************************************************************************/
-
 #include "deviceInfo.h"
 #include "byteStream.h"
 #include "rpcLib.h"
